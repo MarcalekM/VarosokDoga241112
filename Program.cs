a5a133d87127daa104f9d0cbffc19e95850f2e56
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-11-12 13:56:12 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Varosok;
+
+List<Varos> varosok = new(); 
+using StreamReader sr = new(
+    path: @"../../../src/varosok.csv",
+    encoding: System.Text.Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream) varosok.Add(new(sr.ReadLine()));
+//foreach (var varos in varosok) Console.WriteLine(varos);
+
+var f1 = varosok
+    .Where(v => v.OrszagNev.Equals("Kína"))
+    .Sum(v => v.Nepesseg);
+Console.WriteLine($"1. feladat - A kínai városok népessége: {f1} millió fő");
+
+var f2 = varosok
+    .Where(v => v.OrszagNev.Equals("India"))
+    .Average(v => v.Nepesseg);
+Console.WriteLine($"\n2. feladat - Az indiai városok átlag népessége: {f2} millió fő");
+
+var f3 = varosok.MaxBy(v => v.Nepesseg);
+Console.WriteLine($"\n3. feladat - A legnépesebb nagyváros: {f3.VarosNev}");
+
+var f4 = varosok.
+    Where(v => v.Nepesseg > 20)
+    .OrderByDescending(v => v.Nepesseg);
+Console.WriteLine("\n4. feladat - A 20 millió főnél több lakost számláló városok:");
+foreach (var v in f4) Console.WriteLine($"\t{v.ToString()}");
+
+var f5 = varosok
+    .GroupBy(v => v.OrszagNev)
+    .OrderBy(v => v.Key)
+    .Where(v => v.Count() >= 2)
+    .Count();
+Console.WriteLine($"\n5. feladat - {f5} darab olyan ország van, ami több várossal is képiseli magát a listán");
+
+var f6 = varosok
+    .GroupBy(v => v.VarosNev[0])
+    .OrderBy(v => v.Key)
+    .OrderByDescending(v => v.Count())
+    .First().Key;
+Console.WriteLine($"\n6. feladat - A leggyakoribb városnév kezdő betű: {f6}");
---
+++ Varos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varosok
+{
+    internal class Varos
+    {
+        public string VarosNev {  get; set; }
+        public string OrszagNev { get; set; }
+        public float Nepesseg { get; set; }
+
+        public override string ToString() => $"{VarosNev} {OrszagNev} {Nepesseg}";
+
+        public Varos(string sor)
+        {
+            var temp = sor.Split(';');
+            VarosNev = temp[0];
+            OrszagNev = temp[1];
+            Nepesseg = float.Parse(temp[2]);
+        }
+    }
+}
